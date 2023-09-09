17d31cc104d7dc73be0532ef43e71120c3911282
BRStuff <brstuff3000@gmail.com>
2023-09-09 01:54:14 +0100

    Converted to luma.oled Library and sh1106 Oled

--- stats.py
+++ stats.py
@@ -1,74 +1,84 @@
-# Created by: Michael Klements
-# For Raspberry Pi Desktop Case with OLED Stats Display
-# Base on Adafruit CircuitPython & SSD1306 Libraries
-# Installation & Setup Instructions - https://www.the-diy-life.com/add-an-oled-stats-display-to-raspberry-pi-os-bullseye/
 import time
-import board
-import busio
-import digitalio
-
-from PIL import Image, ImageDraw, ImageFont
-import adafruit_ssd1306
-
 import subprocess
+import RPi.GPIO as GPIO
+from luma.core.interface.serial import i2c
+from luma.oled.device import sh1106
+from PIL import Image, ImageDraw, ImageFont
 
-# Define the Reset Pin
-oled_reset = digitalio.DigitalInOut(board.D4)
+# Configuração dos pinos GPIO
+GPIO.setmode(GPIO.BCM)
+button_pin = 17  # Substitua pelo número do pino GPIO que você está usando
+GPIO.setup(button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-# Display Parameters
+# Define o objeto de série I2C
+serial = i2c(port=1, address=0x3C)
+
+# Configuração do display
 WIDTH = 128
 HEIGHT = 64
-BORDER = 5
 
-# Display Refresh
-LOOPTIME = 1.0
+# Inicialize o dispositivo OLED SSH1106
+device = sh1106(serial, width=WIDTH, height=HEIGHT, rotate=0)
 
-# Use for I2C.
-i2c = board.I2C()
-oled = adafruit_ssd1306.SSD1306_I2C(WIDTH, HEIGHT, i2c, addr=0x3C, reset=oled_reset)
+# Crie uma imagem em branco para desenhar
+image = Image.new("1", (WIDTH, HEIGHT))
 
-# Clear display.
-oled.fill(0)
-oled.show()
-
-# Create blank image for drawing.
-# Make sure to create image with mode '1' for 1-bit color.
-image = Image.new("1", (oled.width, oled.height))
-
-# Get drawing object to draw on image.
+# Obtenha o objeto de desenho para desenhar na imagem
 draw = ImageDraw.Draw(image)
 
-# Draw a white background
-draw.rectangle((0, 0, oled.width, oled.height), outline=255, fill=255)
-
+# Crie uma fonte
 font = ImageFont.truetype('PixelOperator.ttf', 16)
-#font = ImageFont.load_default()
 
-while True:
+def display1():
+    draw.rectangle((0, 0, WIDTH, HEIGHT), outline=0, fill=0)
 
-    # Draw a black filled box to clear the image.
-    draw.rectangle((0, 0, oled.width, oled.height), outline=0, fill=0)
-
-    # Shell scripts for system monitoring from here : https://unix.stackexchange.com/questions/119126/command-to-display-memory-usage-disk-usage-and-cpu-load
-    cmd = "hostname -I | cut -d\' \' -f1"
-    IP = subprocess.check_output(cmd, shell = True )
+    # Shell scripts para monitoramento do sistema
+    cmd = "hostname -I | cut -d' ' -f1"
+    IP = subprocess.check_output(cmd, shell=True).decode("utf-8")
     cmd = "top -bn1 | grep load | awk '{printf \"CPU: %.2f\", $(NF-2)}'"
-    CPU = subprocess.check_output(cmd, shell = True )
+    CPU = subprocess.check_output(cmd, shell=True).decode("utf-8")
     cmd = "free -m | awk 'NR==2{printf \"Mem: %s/%sMB %.2f%%\", $3,$2,$3*100/$2 }'"
-    MemUsage = subprocess.check_output(cmd, shell = True )
+    MemUsage = subprocess.check_output(cmd, shell=True).decode("utf-8")
     cmd = "df -h | awk '$NF==\"/\"{printf \"Disk: %d/%dGB %s\", $3,$2,$5}'"
-    Disk = subprocess.check_output(cmd, shell = True )
+    Disk = subprocess.check_output(cmd, shell=True).decode("utf-8")
     cmd = "vcgencmd measure_temp |cut -f 2 -d '='"
-    Temp = subprocess.check_output(cmd, shell = True )
+    Temp = subprocess.check_output(cmd, shell=True).decode("utf-8")
 
-    # Pi Stats Display
-    draw.text((0, 0), "IP: " + str(IP,'utf-8'), font=font, fill=255)
-    draw.text((0, 16), str(CPU,'utf-8') + "LA", font=font, fill=255)
-    draw.text((80, 16), str(Temp,'utf-8') , font=font, fill=255)
-    draw.text((0, 32), str(MemUsage,'utf-8'), font=font, fill=255)
-    draw.text((0, 48), str(Disk,'utf-8'), font=font, fill=255)
-        
-    # Display image
-    oled.image(image)
-    oled.show()
-    time.sleep(LOOPTIME)
+    # Exibir estatísticas do Pi no display
+    draw.text((0, 0), "IP: " + IP, font=font, fill=255)
+    draw.text((0, 16), CPU + " LA", font=font, fill=255)
+    draw.text((80, 16), Temp, font=font, fill=255)
+    draw.text((0, 32), MemUsage, font=font, fill=255)
+    draw.text((0, 48), Disk, font=font, fill=255)
+
+    # Exibir a imagem no display
+    device.display(image)
+
+
+def display2():
+    draw.rectangle((0, 0, WIDTH, HEIGHT), outline=0, fill=0)
+    draw.text((0, 48), "teste", font=font, fill=255)
+
+    # Exibir a imagem no display
+    device.display(image)
+
+
+
+current_function = display1
+while True:
+    # Verifica se o botão foi pressionado
+    if GPIO.input(button_pin) == GPIO.LOW:
+        # Alterna entre as funções
+        if current_function == display1:
+            current_function = display2
+        else:
+            current_function = display1
+
+    # Chama a função atual
+    current_function()
+
+    # Aguarda um pequeno intervalo para evitar detecções múltiplas
+    time.sleep(1)
+
+
+GPIO.cleanup()
